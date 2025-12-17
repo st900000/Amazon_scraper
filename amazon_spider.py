a2ace1ebfacd7ec56fc524c99aaf928414846f45
Stega <ezeastephen3@gmail.com>
2025-12-17 21:03:50 +0100

    Refactor AmazonSpiderSpider for clarity

--- amazon_spider.py
+++ amazon_spider.py
@@ -41,21 +41,17 @@ class AmazonSpiderSpider(scrapy.Spider):
 
         sel = Selector(text=html)
 
-        # Extract all product URLs
         product_links = sel.css('a.a-link-normal.s-no-outline::attr(href)').getall()
 
         for href in product_links:
             full_url = response.urljoin(href)
             yield scrapy.Request(full_url, callback=self.parse_product)
 
-        # Find NEXT PAGE link
         next_page = sel.css("a.s-pagination-next::attr(href)").get()
 
         if next_page:
             next_url = response.urljoin(next_page)
 
-            # next pages DO NOT need Playwright again unless Amazon blocks HTML requests
-            # but to be safe we use Playwright for all Amazon pages
             yield scrapy.Request(
                 next_url,
                 meta={
@@ -71,7 +67,6 @@ class AmazonSpiderSpider(scrapy.Spider):
             )
 
     def parse_product(self, response):
-        # Extract product details from product page
         amazon_item = AmazonscraperItem()
         amazon_item['description'] = response.css("#productTitle::text").get(),
         amazon_item['price'] = response.css("span.a-price-whole::text").get(),
@@ -83,3 +78,4 @@ class AmazonSpiderSpider(scrapy.Spider):
         page = failure.request.meta.get("playwright_page")
         if page:
             await page.close()
+
